277a31580e9ae9c3b2e8ff2332a7e6cc83533369
Susan Zhao <susanmz@gl1000.arc-ts.umich.edu>
2023-11-29 23:59:12 -0500

    Trained model (best.pt) using roboflow

--- yolo_fresh.py
+++ yolo_fresh.py
@@ -10,9 +10,9 @@
 # # results = model.train(task='detect', data='C:\\Users\\susan\\Documents\\_UM\\23Fall\\EECS504\\cv_freshness\\data.yaml', epochs=8, imgsz=640)
 from ultralytics import YOLO
 from roboflow import Roboflow
-# rf = Roboflow(api_key="nLllOIGdCVe8CBQ0w1qB")
-# project = rf.workspace("college-74jj5").project("freshness-fruits-and-vegetables")
-# dataset = project.version(7).download("yolov8")
+#rf = Roboflow(api_key="nLllOIGdCVe8CBQ0w1qB")
+#project = rf.workspace("college-74jj5").project("freshness-fruits-and-vegetables")
+#dataset = project.version(7).download("yolov8")
 
 # Load a model
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
@@ -21,5 +21,5 @@ model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer 
 
 # Train the model
 # results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
-results = model.train(data=f'./freshness--fruits-and-vegetables-7/data.yaml', epochs=300, imgsz=800, plots=True)
+results = model.train(data=f'./freshness--fruits-and-vegetables-7/data.yaml', epochs=30, imgsz=800, plots=True)
 # yolo task=detect mode=train model=yolov8s.pt data={dataset.location}/data.yaml epochs=25 imgsz=800 plots=True
