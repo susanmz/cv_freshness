302fd822e3a4c8ba597e8c90c48c614ee5c95eeb
Susan Zhao <susanoahz@gmail.com>
2023-12-03 14:02:19 -0500

    Files necessary to train on kaggle

---
+++ train_kaggle.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import os
+from IPython.display import display, Image
+from IPython import display
+display.clear_output
+# # ! yolo checks
+
+# # ! yolo task=detect mode=train model=yolov8x.pt data=data.yaml epochs=8 
+model = YOLO('yolov8n.yaml')  # build a new model from YAML
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+results = model.train(task='detect', data='./data.yaml', epochs=8, imgsz=640)
+
+
+# pip install ultralytics
+
+# from ultralytics import YOLO
+# import os
+# from IPython.display import display, Image
+# from IPython import display
+# display.clear_output
+# ! yolo checks
+
+# ! yolo predict model=yolov8n.pt source='https://ultralytics.com/images/bus.jpg'
+
+# ! yolo task=detect mode=train model=yolov8n.pt data=/content/drive/MyDrive/data.yaml epochs=8 imgsz=640
