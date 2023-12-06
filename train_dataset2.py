0c4f564e25797cb70cbf6494a803725794b9a278
Susan Zhao <susanoahz@gmail.com>
2023-12-06 15:29:38 -0500

    dataset2 info

---
+++ train_dataset2.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import os
+from IPython.display import display, Image
+from IPython import display
+display.clear_output
+# # ! yolo checks
+
+# # ! yolo task=detect mode=train model=yolov8x.pt data=data.yaml epochs=8 
+model = YOLO('yolov8n.yaml')  # build a new model from YAML
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+results = model.train(task='detect', data='./dataset2_data.yaml', epochs=1, imgsz=640)
+
