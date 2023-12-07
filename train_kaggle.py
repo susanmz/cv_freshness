628f8280963d3692586f7f872a5e0de26896d33f
Rohit Banerjee <rohitban@gl1501.arc-ts.umich.edu>
2023-12-06 23:43:04 -0500

    Exps new

--- train_kaggle.py
+++ train_kaggle.py
@@ -9,7 +9,7 @@ display.clear_output
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
-results = model.train(task='detect', data='./kaggle_data.yaml', epochs=1, imgsz=640)
+results = model.train(task='detect', data='./kaggle_data.yaml', epochs=30, imgsz=640)
 
 
 # pip install ultralytics
